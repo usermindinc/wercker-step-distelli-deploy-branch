e38c8dbb09aefae052e235e8f2648103672a9d19
Eric Maland <emaland@usermind.com>
2014-08-19 16:41:12 -0700

    test actual output of list releases; version bump

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ R_ID=""
 if [ "$WERCKER_GIT_BRANCH" == "master" ]; then 
   
   echo -e "Running:$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2"
+  $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2
   R_ID=`$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2`
   echo -e "Release ID: $R_ID"
   if [ -n "$R_ID" ] 
