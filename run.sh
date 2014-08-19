c7a71b79d4b51777a5a9bc12bcf67dba32f8f224
Eric Maland <emaland@usermind.com>
2014-08-19 16:24:28 -0700

    add more verbose output to determine what behaviour is occurring

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 set -v
-rm ~/.bash_logout
+rm -f ~/.bash_logout
 R_ID=""
 if [ "$WERCKER_GIT_BRANCH" == "master" ]; then 
-  R_ID=$($WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep "$WERCKER_GIT_COMMIT" | tail -n 1 | cut -d',' -f2)
+  CMD="$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2)"
+  echo "Running: $CMD"
+  R_ID=$(CMD)
+  echo "Release ID: $R_ID"
   if [ -n "$R_ID" ] 
   then 
     $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli deploy -q -y -r $R_ID -m "Wercker deploy of $DISTELLI_APP to $DISTELLI_ENV" -e $DISTELLI_ENV
