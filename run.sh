b300f054460d3f90a5640f07d23914cc4602e7bf
Eric Maland <emaland@usermind.com>
2014-08-19 16:27:18 -0700

    add more verbose output to determine what behaviour is occurring

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ R_ID=""
 if [ "$WERCKER_GIT_BRANCH" == "master" ]; then 
   CMD="$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2)"
   echo "Running: $CMD"
-  R_ID=$(CMD)
+  R_ID=$($CMD)
   echo "Release ID: $R_ID"
   if [ -n "$R_ID" ] 
   then 
