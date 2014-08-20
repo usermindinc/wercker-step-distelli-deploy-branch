98cc40b1326be631197892b2e78c30627a138c16
Eric Maland <emaland@usermind.com>
2014-08-20 11:07:09 -0700

    add options and README for deploy step

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 set -v
 rm -f ~/.bash_logout
-R_ID=""
-if [ "$WERCKER_GIT_BRANCH" == "master" ]; then 
-  
+
+R_ID="$WERCKER_DISTELLI_DEPLOY_BRANCH_RELEASE_ID"
+DEPLOY_BRANCH="master"
+
+if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_BRANCH" ]; then
+  DEPLOY_BRANCH = "$WERCKER_DISTELLI_DEPLOY_BRANCH_BRANCH"
+fi
+
+if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_APP" ]; then 
+  DISTELLI_APP = $WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_APP
+fi
+
+if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV" ]; then
+  DISTELLI_ENV = $WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV
+fi
+
+if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
   echo -e "Running:$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2"
   $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2
-  R_ID=`$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2`
+  if [ ! -n "$R_ID" ]; then 
+    R_ID=`$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2`
+  fi
   echo -e "Release ID: $R_ID"
   if [ -n "$R_ID" ] 
   then 
