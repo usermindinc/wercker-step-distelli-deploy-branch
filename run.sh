8c89b9be1b3396448ad76cc88bcf7978d565f8da
Eric Maland <emaland@usermind.com>
2014-08-21 12:12:42 -0700

    debug for deploy failures

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 set -v
+function deploy {
 
 R_ID="$WERCKER_DISTELLI_DEPLOY_BRANCH_RELEASE_ID"
 DEPLOY_BRANCH="master"
 
 if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_BRANCH" ]; then
-  DEPLOY_BRANCH = "$WERCKER_DISTELLI_DEPLOY_BRANCH_BRANCH"
+  DEPLOY_BRANCH="$WERCKER_DISTELLI_DEPLOY_BRANCH_BRANCH"
 fi
 
 if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_APP" ]; then 
-  DISTELLI_APP = $WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_APP
+  DISTELLI_APP=$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_APP
 fi
 
 if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV" ]; then
-  DISTELLI_ENV = $WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV
+  DISTELLI_ENV=$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV
 fi
 
-#if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
-if [ "" ]; then
+if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
   echo -e "Running:$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2"
   $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2
   if [ ! -n "$R_ID" ]; then 
@@ -33,3 +33,8 @@ if [ "" ]; then
     exit 1
   fi
 fi
+
+}
+
+deploy
+echo "Exit Code: " $?
