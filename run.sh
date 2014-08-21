f914f9d5e37af729bc8e4b206a4711e1918d3a0d
Eric Maland <emaland@usermind.com>
2014-08-21 11:43:47 -0700

    debug for deploy failures

--- run.sh
+++ run.sh
@@ -15,6 +15,19 @@ if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV" ]; then
   DISTELLI_ENV = $WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV
 fi
 
-if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
-echo bananas
+#if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
+if [ "" ]; then
+  echo -e "Running:$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2"
+  $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2
+  if [ ! -n "$R_ID" ]; then 
+    R_ID=`$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2`
+  fi
+  echo -e "Release ID: $R_ID"
+  if [ -n "$R_ID" ] 
+  then 
+    $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli deploy -q -y -r $R_ID -m "Wercker deploy of $DISTELLI_APP to $DISTELLI_ENV" -e $DISTELLI_ENV
+  else 
+    echo "No release found for $DISTELLI_ENV/$DISTELLI_APP at $WERCKER_GIT_BRANCH"
+    exit 1
+  fi
 fi
