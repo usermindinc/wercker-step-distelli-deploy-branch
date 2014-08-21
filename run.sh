b381eb3eac9166f817b5c66af1d52f4e11c76d8b
Eric Maland <emaland@usermind.com>
2014-08-21 11:37:27 -0700

    debug for deploy failures

--- run.sh
+++ run.sh
@@ -16,17 +16,5 @@ if [ -n "$WERCKER_DISTELLI_DEPLOY_BRANCH_DISTELLI_ENV" ]; then
 fi
 
 if [ "$WERCKER_GIT_BRANCH" == "$DEPLOY_BRANCH" ]; then
-  echo -e "Running:$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2"
-  echo $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2
-  if [ ! -n "$R_ID" ]; then 
-    echo foo
-    # R_ID=`$WERCKER_CACHE_DIR/DistelliCLI/bin/distelli list releases -n $DISTELLI_APP -f csv | grep $WERCKER_GIT_COMMIT | tail -n 1 | cut -d',' -f2`
-  fi
-  echo -e "Release ID: $R_ID"
-  if [ -n "$R_ID" ] 
-  then 
-    echo $WERCKER_CACHE_DIR/DistelliCLI/bin/distelli deploy -q -y -r $R_ID -m "Wercker deploy of $DISTELLI_APP to $DISTELLI_ENV" -e $DISTELLI_ENV
-  else 
-    echo "No release found for $DISTELLI_ENV/$DISTELLI_APP at $WERCKER_GIT_BRANCH"
-  fi
+echo bananas
 fi
